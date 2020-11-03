f409bba2bb86ad98159dc617d5f80d5b623ba8ba
sowrerajan <rajansowre@gmail.com>
2020-11-03 14:54:15 +0530

    START APP

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Component_homepage from './Components/Component_homepage';
+
+function App() {
+  return (
+    <div className='app'>
+      <Component_homepage />
+   </div>
+ )
+}
+
+export default App;
